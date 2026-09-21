78903d6bca0b40c291e492fc6b7276d2e7e33ac0
Jonathan Hernández <ingehernangonz505@gmail.com>
2026-09-21 16:22:09 -0600

    Main error - descartar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './Core/rutas_navegacion.dart';
+import 'Screens/login/widget/w_login.dart';
 
 void main() {
   runApp(const App_Veterinaria());
@@ -13,7 +14,7 @@ class App_Veterinaria extends StatelessWidget {
     return MaterialApp(
       initialRoute: RutasNavegacion.login,
       routes: {
-        //RutasNavegacion.login: (context) => const LoginScreen(), 
+        RutasNavegacion.login: (context) => loginScreen(),
         //RutasNavegacion.inicio: (context) => const InicioScreen(),
         //RutasNavegacion.inventario: (context) => const InventarioScreen(),
         //RutasNavegacion.ventas: (context) => const VentasScreen(),
