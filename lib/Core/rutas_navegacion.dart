cc166f365d668d36f7cee9aca2b0d2663541252f
Darell Jimenez <jabrielthj07@gmail.com>
2026-09-21 13:51:34 -0600

    core: Creando Carpeta Coro y carpeta de rutas

--- lib/Core/rutas_navegacion.dart
+++ lib/Core/rutas_navegacion.dart
@@ -1,7 +1,7 @@
 class RutasNavegacion {
-static const String login = '/';
+static const String login = '/login'; // cambiar a solo / despues 
 static const String inicio = '/Inicio';
-static const String inventario = '/inventario';
+static const String inventario = '/';
 static const String ventas = '/Ventas';
 static const String perfil = '/Perfil';
 }
