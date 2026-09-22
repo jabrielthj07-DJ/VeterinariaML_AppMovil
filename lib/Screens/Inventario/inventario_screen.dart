8f8a91042aadfcd72c3c34db8eb22915e3da70d6
Darell Jimenez <jabrielthj07@gmail.com>
2026-09-21 22:05:26 -0600

    fix(code and ui): Corrigiendo estructura interfaz Inventario

--- lib/Screens/Inventario/inventario_screen.dart
+++ lib/Screens/Inventario/inventario_screen.dart
@@ -1,38 +1,28 @@
 import 'package:flutter/material.dart';
 
-class InventarioScreen extends StatefulWidget {
+// Widgets
+import './widget/tarjetas.dart';
+
+
+class InventarioScreen extends StatelessWidget {
   const InventarioScreen({super.key});
-
-  @override
-  State<InventarioScreen> createState() => _InventarioScreenState();
-}
-
-class _InventarioScreenState extends State<InventarioScreen> {
-  int currentPageIndex = 0;
-  NavigationDestinationLabelBehavior labelBehavior = .alwaysShow;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        labelBehavior: labelBehavior,
-        selectedIndex: currentPageIndex,
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        destinations: const <Widget>[
-          NavigationDestination(icon: Icon(Icons.home), label: 'Inicio'),
-           NavigationDestination(icon: Icon(Icons.analytics_outlined), label: 'Ventas'),
-          NavigationDestination(icon: Icon(Icons.backpack_outlined), label: 'Inventario'),
-          NavigationDestination(icon: Icon(Icons.account_circle_outlined), label: 'Perfil'),
-        ],
+  appBar: AppBar(
+        title: const Text('Demo 1 Inventario'),
       ),
-      body: Center(
-        child: Text('Pagina Inventario $currentPageIndex'),
-      ),
-      );
-    }
 
+      body: Column(
+      children: [
+        // buscador
+        // filtros
+        const TarjetaProduct(),
+        // etc.
+      ],
+      )
+    );
+    
+  }
 }
