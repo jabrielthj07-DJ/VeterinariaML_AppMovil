47208388367c4bd1f8b772358a1902d099efef8c
Darell Jimenez <jabrielthj07@gmail.com>
2026-09-26 11:49:07 -0600

    Feat(Ui): Creando Segmentacion de Ventas

--- lib/Screens/ventas/ventas_screen.dart
+++ lib/Screens/ventas/ventas_screen.dart
@@ -5,17 +5,26 @@ import './widget/tarjetas.dart';
 import './widget/buscador.dart';
 import './widget/filtro.dart';
 
-class VentasScreen extends StatelessWidget {
+class VentasScreen extends StatefulWidget {
   const VentasScreen({super.key});
+
+   @override
+  State<VentasScreen> createState() => _VentasScreenState();
+}
+
+class _VentasScreenState extends State<VentasScreen> {
+
+  int vistaSeleccionada = 0; // Acordarse q colas y hay dos btn here
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
   appBar: AppBar (
-       
-        title: const Text('                           Historial Ventas',
+      backgroundColor: const Color(0xFF087A4B),
+      foregroundColor: Colors.white,
+        title: const Text('                     Analisis de Ventas',
         style: TextStyle(
-        color: const Color.fromARGB(155, 18, 52, 79),
+        color: Color.fromARGB(255, 247, 250, 252),
         fontFamily: 'sans-serif',
         fontSize: 18,
         fontWeight: FontWeight.bold,
@@ -27,7 +36,36 @@ class VentasScreen extends StatelessWidget {
       children: [
 
         const SizedBox(height: 14),
+            
+      SegmentedButton<int>(
+      segments: const [
+        ButtonSegment(
+          value: 0,
+          label: Text('Reporte'),
+        ),
+        ButtonSegment(
+          value: 1,
+          label: Text('Historial'),
+        ),
+      ],
+      selected: {vistaSeleccionada},
+      onSelectionChanged: (Set<int> seleccion) {
+        setState(() {
+          vistaSeleccionada = seleccion.first;
+        });
+      },
+    ),
+
+        const SizedBox(height: 8),
+       
+        if (vistaSeleccionada == 0)
+        const Text('Dashboard prueba') // Aqui recordar agregar power bi Reporte, acordarse que esta en trelo dise;o de figma
+        else
         const BuscadorVenta(),
+
+      
+    
+
         const SizedBox(height: 8),
         const Filtro(),
         const SizedBox(height: 8),
